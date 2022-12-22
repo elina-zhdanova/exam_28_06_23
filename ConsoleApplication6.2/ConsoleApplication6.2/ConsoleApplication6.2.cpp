4591549af06b8e80146791814411bdacf29e1a02
elina-zhdanova <elizhdanova@yandex.ru>
2022-12-22 22:37:43 +0300

    C:\WorkDir\labs-elina-zhdanova\ConsoleApplication6.2

---
+++ ConsoleApplication6.2/ConsoleApplication6.2/ConsoleApplication6.2.cpp
@@ -0,0 +1,34 @@
+﻿#include <deque>
+#include <iostream>
+using namespace std;
+#include <string>
+
+bool pal(string str)
+{
+    deque <char> pol;
+    bool flag = true;
+    for (int i = 0; i < str.length(); i++) str[i] = tolower(str[i]);
+    for (int i = 0; i < str.length(); i++) pol.push_back(str[i]);
+    for (int i = 0; i < (str.length())/2; i++)
+    {
+        
+        if (pol.back() == pol.front())
+        {
+            flag = true;
+            pol.pop_front();
+            pol.pop_back();
+        }
+        else return 0;
+
+    }
+    return 1;
+}
+
+
+int main()
+{
+    string str;
+    getline(cin, str);
+    cout << pal(str);
+    return 0;
+}
