d0618cbe2ad9adb6b136d061b262fc26d3c7102d
elina-zhdanova <elizhdanova@yandex.ru>
2022-12-30 10:33:17 +0300

    C:\WorkDir\labs-elina-zhdanova\ConsoleApplication6.6

---
+++ ConsoleApplication6.6/ConsoleApplication6.6.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    string word;
+    int k =0 ;
+    map<string, int> mm;
+    while (cin >> word) // конец ввода ctrl^z потом enter
+    {
+        mm[word]++; // считаем слова
+        k++; //всего слов
+    }
+
+    map<string, int>::iterator it; //явный итератор 
+    for (it = mm.begin(); it != mm.end(); ++it) 
+        cout << it->first << ':' << double (it->second)/k * 100 << endl;//first-обращение к ключу[word],
+    //second-обращаемся к ячейке(к количеству 
+    //повторяющихся слов mm
+    //преобразуем в double ибо второе число целое, а нам нужны проценты, поделится криво
+    system("pause");
+    return 0;
+}
