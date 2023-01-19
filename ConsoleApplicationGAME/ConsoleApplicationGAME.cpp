b836d0685d43cba82d7ed6eb47d1bb26be45e64f
elina-zhdanova <elizhdanova@yandex.ru>
2023-01-19 13:50:12 +0300

    C:\WorkDir\labs-elina-zhdanova\ConsoleApplicationGAME

--- ConsoleApplicationGAME/ConsoleApplicationGAME.cpp
+++ ConsoleApplicationGAME/ConsoleApplicationGAME.cpp
@@ -1,1 +1,22 @@
-﻿
+﻿#include <iostream>
+#include "game_Zhdanova.h"
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int choise;
+	int kol;
+	while (true)
+	{
+		cout<<"Выберите действие:\n"
+			<< "1. Ввести число игроков\n"
+			<< "2. Ввести имена\n"
+			<< "3.
+			<< "Ваш выбор: ";
+		cin >> choise;
+		switch (choise)
+		{
+
+}
--- ConsoleApplicationGAME/game_Zhdanova.h
+++ ConsoleApplicationGAME/game_Zhdanova.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <iostream>
+#include <stdlib.h> // нужен для вызова функций rand(), srand()
+#include <time.h> // нужен для вызова функции time()
 #include <vector>
 using namespace std;
 
@@ -7,31 +9,31 @@ class game_Zhdanova
 {
 private:
 	int kol;
-	struct Player_stats 
-	{
+	struct Player_stats {
 		string name;
-		int gold, tree, rock, tokens, cube1, cube2, cube3, dop_cube;
+		int gold = 0, tree = 0, rock, tokens, cube1, cube2, cube3, dop_cube;
 		vector <string> buildings;
-		int military_power=0;
-		int win_socker=0;
+		int military_power = 0;
+		int win_socker = 0;
 	};
 
-	vector<pair<string,string>> advisors;
+	vector<pair<string, string>> advisors;
 	int year;
 	int phase;
-	string enemy; // враг может быть и структура пока не понял 
+	char enemy; // враг может быть и структура
 	vector <Player_stats> list;
+	int rand_g();
 
 public:
 
 	game_Zhdanova();
-	game_Zhdanova(const game_Zhdanova& game);
+	game_Zhdanova(const game_Zhdanova& temp);
 	~game_Zhdanova();
 	game_Zhdanova(int kol);
 	void phase1();
 	void phase3();
 	void phase5();
 	void phase7();
-	void phase9();
-	void defense_level(const char* enemy_name, int king_help, SomeDataStructForPlaye r& player);
+	void phase8();
+	void defense_level(char enemy_name, int king_help);
 };
